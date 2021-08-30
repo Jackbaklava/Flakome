cde0d10f07e0f57485d056c69669a580c6149523
Jackbaklava <mhqadir14@gmail.com>
2021-08-30 09:31:08 +0100

    Created database models for User and Post, forms for login and sign-up, and flashed messages can now be seen on page.

--- website/models.py
+++ website/models.py
@@ -1,8 +1,15 @@
-# from . import db
+from . import db
 
-# class User(db.Model):
-#     pass
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(128))
+    posts = db.relationship("Post")
 
 
-# class Post(db.Model):
-#     pass
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    title = db.Column(db.String(128))
+    body = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True))
