969532ab3f9133fb1d57c97f024aa602c11d49c0
Jackbaklava <mhqadir14@gmail.com>
2021-09-02 20:50:08 +0100

    Started work on create post form

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 
@@ -9,3 +9,12 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     return render_template("home.html")
+
+
+@views.route("create-post", methods=["GET", "POST"])
+@login_required
+def create_post():
+    if request.method == "POST":
+        pass
+    
+    return render_template("create-post.html")
