6177dc11ce6685d095851e28bfeb16a59cec3edc
Jackbaklava <mhqadir14@gmail.com>
2021-09-07 06:11:10 +0100

    Exceeding character limits when posting saves your work before flashing error message.

--- website/views.py
+++ website/views.py
@@ -14,30 +14,36 @@ def home():
     return render_template("home.html")
 
 
-@views.route("create-post", methods=["GET", "POST"])
+@views.route("/create-post", methods=["GET", "POST"])
 @login_required
-def create_post():
+def create_post(title="", body=""):
     if request.method == "POST":
         data = request.form
-        title = data.get("title")
-        body = data.get("body")
+        given_title = data.get("title")
+        given_body = data.get("body")
 
         title_limits = CharLimits.post["title"]
         body_limits = CharLimits.post["body"]
 
-        if len(title) < title_limits["min"] or len(title) > title_limits["max"]:
-            flash(f"Title must be between {title_limits['min']} and {title_limits['max']} characters long.", category="error")
+        if (len(given_title) < title_limits["min"] or len(given_title) > title_limits["max"]):
+            flash(f"Title must be between {title_limits['min']} and {title_limits['max']} characters long.",
+                category="error")
 
-        elif len(body) < body_limits["min"] or len(body) > body_limits["max"]:
-            flash(f"Body must be between {body_limits['min']} and {body_limits['max']} characters long.", category="error")
+        elif (len(given_body) < body_limits["min"] or len(given_body) > body_limits["max"]):
+            flash(f"Body must be between {body_limits['min']} and {body_limits['max']} characters long.",
+                category="error")
 
         else:
-            new_post = Post(title=title, body=body, author=current_user)
+            new_post = Post(title=given_title, body=given_body, author=current_user)
             db.session.add(new_post)
             db.session.commit()
             flash("Post created.", category="success")
             return redirect(url_for("views.home"))
 
-        return redirect(url_for("views.create_post"))
-    
-    return render_template("create-post.html")
+        return redirect(url_for("views.create_post", title=given_title, body=given_body))
+
+    return render_template(
+        "create-post.html",
+        title_to_display=request.args.get("title"),
+        body_to_display=request.args.get("body"),
+    )
