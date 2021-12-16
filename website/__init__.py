87cf0464ef2f22b0fc53746fa2df4402c206d4f6
Jackbaklava <mhqadir14@gmail.com>
2021-12-16 19:15:42 +0500

    Db can now be created from console

--- website/__init__.py
+++ website/__init__.py
@@ -10,37 +10,27 @@ load_dotenv()
 db = SQLAlchemy()
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+from .views import views
+from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User
-
-    # create_database(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.login_message_category = "error"
-    login_manager.init_app(app)
+from .models import User
 
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(user_id)
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.login_message_category = "error"
+login_manager.init_app(app)
 
 
-    return app
-
-
-# def create_database(app):
-#     if not os.path.exists("website/" + DB_NAME):
-#         db.create_all(app=app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
